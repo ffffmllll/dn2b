a31e5329192f71c88f799b8080fa28dcb97459b3
ffffmllll <77573741+ffffmllll@users.noreply.github.com>
2024-04-09 11:20:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,17 @@ const videos = {
     3: "video3.mp4"
 };
 
-// Function to show initial image
-function showInitialImage() {
-    document.getElementById("initialImage").style.display = "block";
-    document.getElementById("fullScreenVideo").style.display = "none";
-}
+// Show initial image when page loads
+document.getElementById("initialImage").style.display = "block";
+document.getElementById("fullScreenVideo").style.display = "none";
 
-// Function to show video based on key press
-function showVideo(keyPressed) {
+// Listen for keydown events
+document.addEventListener("keydown", function(event) {
+    const keyPressed = event.key;
     const videoElement = document.getElementById("fullScreenVideo");
     if (videos.hasOwnProperty(keyPressed)) {
         document.getElementById("initialImage").style.display = "none";
         videoElement.style.display = "block";
         videoElement.src = videos[keyPressed];
     }
-}
-
-// Show initial image when page loads
-showInitialImage();
-
-// Listen for keydown events
-document.addEventListener("keydown", function(event) {
-    const keyPressed = event.key;
-    showVideo(keyPressed);
 });
